517fc860e6baf654c52b3df1f4300a5095076930
pea <pepea.23@live.com>
2018-12-16 15:15:52 +0700

    [add] FaceBookAuth

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react'
 
-class componentName extends Component {
-  render() {
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '293604811359850',
+    cookie: true,
+    xfbml: true,
+    version: 'v3.2'
+  })
+
+  FB.AppEvents.logPageView()
+};
+
+(function (d, s, id) {
+  var js; var fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) { return }
+  js = d.createElement(s); js.id = id
+  js.src = 'https://connect.facebook.net/en_US/sdk.js'
+  fjs.parentNode.insertBefore(js, fjs)
+}(document, 'script', 'facebook-jssdk'))
+
+class componentName extends React.Component {
+  render () {
     return (
       <div>
-        gi
       </div>
-    );
+    )
   }
 }
 
 componentName.propTypes = {
 
-};
+}
 
-export default componentName;
+export default componentName
