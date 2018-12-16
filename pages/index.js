644c3842fe153f0d226b4fce9cab6ee4d06e113f
fame <>
2018-12-16 14:44:10 +0700

    [ADD] index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+class componentName extends Component {
+  render() {
+    return (
+      <div>
+        gi
+      </div>
+    );
+  }
+}
+
+componentName.propTypes = {
+
+};
+
+export default componentName;
