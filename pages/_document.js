3d4dbce4cd30f94fc74be70aff2c8de7c25d02d6
pea <pepea.23@live.com>
2018-12-16 15:43:50 +0700

    [add] Ant + BootStarp

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,28 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps (ctx) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
 
-  render() {
+  render () {
     return (
       <html>
         <Head>
+
           <style>{`body { margin: 0 } /* custom! */`}</style>
+          <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/antd/2.9.3/antd.min.css' />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+
         </Head>
         <body className="custom_class">
           <Main />
           <NextScript />
         </body>
+
       </html>
+
     )
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,13 @@
 import React from 'react'
+import LoginPage from './../component/Login'
 
-window.fbAsyncInit = function () {
-  FB.init({
-    appId: '293604811359850',
-    cookie: true,
-    xfbml: true,
-    version: 'v3.2'
-  })
 
-  FB.AppEvents.logPageView()
-};
-
-(function (d, s, id) {
-  var js; var fjs = d.getElementsByTagName(s)[0]
-  if (d.getElementById(id)) { return }
-  js = d.createElement(s); js.id = id
-  js.src = 'https://connect.facebook.net/en_US/sdk.js'
-  fjs.parentNode.insertBefore(js, fjs)
-}(document, 'script', 'facebook-jssdk'))
 
 class componentName extends React.Component {
   render () {
     return (
       <div>
+        <LoginPage></LoginPage>
       </div>
     )
   }
