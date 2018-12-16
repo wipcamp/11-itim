f49bfb12bc04053f547f85a04389b66f90f88650
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-16 17:35:31 +0700

    [UPDATE] remove antd cdn

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,6 @@ export default class MyDocument extends Document {
         <Head>
 
           <style>{`body { margin: 0 } /* custom! */`}</style>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.11.2/antd.css" integrity="sha256-Px4z35WMarLJCT3K+yaUCxcGYegwNX2TPBxWaId4Dmg=" crossorigin="anonymous" />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
 
         </Head>
