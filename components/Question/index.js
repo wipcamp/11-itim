3287a84ee5ab4891687e05f041e2ea987641d004
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-16 15:57:14 +0700

    [UPDATE] change components name from component

---
+++ components/Question/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class question extends React.Component {
+  render () {
+    return (
+      <div className="container-fluid">
+        <div className="container">
+          test
+        </div>
+      </div>
+    )
+  }
+}
+
+export default question
