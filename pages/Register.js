6bf2b614a1ac90381c31e2da5d838175456e7e67
pea <pepea.23@live.com>
2018-12-17 13:51:42 +0700

    [ADD] Register Page Route

---
+++ pages/Register.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import RegisterPage from './../components/Register'
+
+class Register extends React.Component {
+  render () {
+    return (
+      <Fragment>
+        <RegisterPage/>
+      </Fragment>
+    )
+  }
+}
+
+export default Register
