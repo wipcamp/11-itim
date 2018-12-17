6b5c5bf78ad5fb9f5d4c6cacd5d261a4f3e76a1b
pea <pepea.23@live.com>
2018-12-17 15:37:52 +0700

    [ADD] YarnBuildTest

--- next.config.js
+++ next.config.js
@@ -1,18 +1,2 @@
-require('dotenv').config()
 const withCSS = require('@zeit/next-css')
-module.exports = {
-  useFileSystemPublicRoutes: false,
-  publicRuntimeConfig: {
-    API_URL: process.env.API_URL,
-    APP_URL: process.env.APP_URL,
-    TIME_ZONE: process.env.TIME_ZONE
-  },
-  serverRuntimeConfig: {
-    JWT_SECRET: process.env.JWT_SECRET,
-    REDIS_HOST: process.env.REDIS_HOST,
-    REDIS_PORT: process.env.REDIS_PORT,
-    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-    ALCHEMIST_TOKEN: process.env.ALCHEMIST_TOKEN
-  }
-
-}
+module.exports = withCSS()
