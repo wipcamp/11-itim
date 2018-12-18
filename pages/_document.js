a9451bbd906a652cd0f321ed598883b5ddf2a429
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-18 12:43:51 +0700

    [UPDATE] ServerStyleSheet in _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,40 @@
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx) {
+    const sheet = new ServerStyleSheet()
+
+    const originalRenderPage = ctx.renderPage
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      })
+
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()]
+    }
   }
 
   render () {
     return (
       <html>
         <Head>
-
           <style>{`body { margin: 0 } /* custom! */`}</style>
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
-
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+            integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+            crossorigin="anonymous"
+          />
         </Head>
         <body className="custom_class">
           <Main />
           <NextScript />
         </body>
-
       </html>
-
     )
   }
 }
