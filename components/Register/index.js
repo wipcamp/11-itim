f2801111189676da68a6a7471538cfdd04d78b6b
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-18 12:44:35 +0700

    [UPDATE] Progressbar in Register Page

--- components/Register/index.js
+++ components/Register/index.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import RegistrationForm from './RegistrationForm'
 import Progressbar from './../Core/Progressbar'
+import axios from 'axios'
 
 class index extends React.Component {
   state = {
     question: [],
     startIndex: 0,
-    pageIndex: 1
+    pageIndex: 0
   }
-  render () {
+  componentDidMount = async () => {
+    let queryQuestion = await axios.get('http://localhost:8882/api/questions')
+    this.setState({
+      question: queryQuestion.data.question
+    })
+  }
+  render() {
     return (
-      <div>
-        <div className="col mt-5 container-fluid">
-          <Progressbar current={this.state.pageIndex} question={this.state.question}/>
+      <div className="container-fluid">
+        <div className="container mt-5">
+          <Progressbar
+            current={this.state.pageIndex}
+            question={this.state.question}
+          />
         </div>
         <RegistrationForm />
-        </div>
+      </div>
     )
   }
 }
