b31b1706a34a9732b3af8cc48a1f8d8b5d754ee2
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-18 12:43:22 +0700

    [ADD] global style for datepicker

---
+++ components/Core/Globalstyle.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  .ant-calendar-month-panel-selected-cell .ant-calendar-month-panel-month {
+    background: #00B2FF !important;
+    color: #fff !important;
+  }
+`
+
+export default GlobalStyle
