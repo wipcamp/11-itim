a8124967eac132838e208dd66d9b190f18dc051a
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-19 02:13:45 +0700

    [ADD] config/colors && config/color (DesignSystem)

---
+++ config/colors.js
@@ -0,0 +1,13 @@
+export default {
+  primary: '#00B2FF',
+  primaryDarker: '#0098DA',
+  primaryHover: '#0098DA',
+  primaryShadow: '',
+  error: '#F5222D',
+  errorShadow: '',
+  grayShadow: '',
+  normalText: '#000000',
+  placeHolderText: '#BFBFBF',
+  borderColor: '#D9D9D9',
+  hover: '#F0F0F0'
+}
