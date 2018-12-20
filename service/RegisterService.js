c8b3160c1bb8d0f3a1b08a949340ecd6b7a01cfc
pea <pepea.23@live.com>
2018-12-20 15:46:23 +0700

    [Update] Header api

--- service/RegisterService.js
+++ service/RegisterService.js
@@ -5,7 +5,7 @@ import apiReg from '../utils/apiRegService'
 const RegisterService = {
   sendRegister: async (request) => {
     console.log(request)
-    await apiReg.put('/profile', request, { 'Bearer': Cookies.gettokenJWTCookie() }).then(respons => {
+    await apiReg.put('/profile', request, 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTU0NTI4MjczMiwiZXhwIjoxNTQ1Mjg2MzMyLCJuYmYiOjE1NDUyODI3MzIsImp0aSI6Ilo2SWtBNmdzVk10V05DeE8iLCJzdWIiOjExMDAwNSwicHJ2IjoiOWFkNTg1NzdmYWI2ZmJjMTU1NDM0ZDM1OTFlZGYxNjRhMTc3NjVhYSJ9.yJ7nYYsj9OjDKQOtxDP4aNT2lfuC97db_Z6FK8r2pjY').then(respons => {
       Cookies.setCookie('tokenJWT', respons.data.token)
     })
   },
@@ -13,7 +13,7 @@ const RegisterService = {
     return apiReg.get('/schools')
   },
   getProfile: async () => {
-    return apiReg.get('/profile')
+    return apiReg.get('/profile', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoXC9sb2dpbiIsImlhdCI6MTU0NTI4MjczMiwiZXhwIjoxNTQ1Mjg2MzMyLCJuYmYiOjE1NDUyODI3MzIsImp0aSI6Ilo2SWtBNmdzVk10V05DeE8iLCJzdWIiOjExMDAwNSwicHJ2IjoiOWFkNTg1NzdmYWI2ZmJjMTU1NDM0ZDM1OTFlZGYxNjRhMTc3NjVhYSJ9.yJ7nYYsj9OjDKQOtxDP4aNT2lfuC97db_Z6FK8r2pjY')
   }
 
 }
