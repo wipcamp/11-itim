05248ad78c8cd3a9f72f198d760210be620f29cc
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-20 20:08:58 +0700

    [UPDATE] button (design system)

--- components/Core/Button.js
+++ components/Core/Button.js
@@ -1,9 +1,27 @@
 import { Button as DefaultButton } from 'antd'
 import styled from 'styled-components'
-import config from '../../config/fonts'
+import fonts from '../../config/fonts'
+import colors from '../../config/colors'
 
-const Button = styled(DefaultButton)`
-  font-size: ${config.paragraph};
+const ButtonPrimary = styled(DefaultButton)`
+  font-size: ${fonts.paragraph};
+  background-color: ${colors.primary};
+  color: #fff;
+  &:hover{
+    background-color: ${colors.primaryDarker};
+    color: #fff;
+  }
 `
 
-export default Button
+export const ButtonSecondary = styled(DefaultButton)`
+  font-size: ${fonts.paragraph};
+  background-color: transparent;
+  color: ${colors.primary};
+  border-color: ${colors.primary};
+  &:hover{
+    background-color: ${colors.primaryDarker};
+    color: ${colors.hover}
+  }
+`
+
+export default ButtonPrimary
