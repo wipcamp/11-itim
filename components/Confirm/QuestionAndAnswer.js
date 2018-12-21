a04291dfde60330581f579a308861cb4bd63de26
fame <thinnapat.2542@mail.kmutt.ac.th>
2018-12-21 18:20:09 +0700

    [UPDATE] getQuestion & Answer

--- components/Confirm/QuestionAndAnswer.js
+++ components/Confirm/QuestionAndAnswer.js
@@ -1,42 +1,33 @@
 import React from 'react'
+import QuestionService from '../../service/QuestionService'
+
 
 class QuestionAndAnswer extends React.Component {
   state = {
-    questions: [
-      {
-        id: 1,
-        content: 'test',
-        answer: 'test'
-      },
-      {
-        id: 2,
-        content: 'test2',
-        answer: 'test2'
-      },
-      {
-        id: 3,
-        content: 'test3',
-        answer: 'test3'
-      },
-      {
-        id: 4,
-        content: 'test4',
-        answer: 'test5'
-      }
-    ]
+    questions:[]
   }
+  componentDidMount = async () => {
+    let queryQuestion = await QuestionService.getAllQuestion()
+    this.setState({
+      questions: queryQuestion.data
+    })
+    console.log(this.state.questions)
+    // for (let index = 0; index < this.state.questions.length; index++) {
+    //   this.state.answers.push({ questionId: index + 1, ans_content: '' })
+    }
   render() {
     return (
       <div className="container-fluid">
         <div className="row justify-content-center">
           <div className="col-10">
+          <h3 className="font-weight-bold mb-4 ml-5">ตอบคำถาม</h3>
             {this.state.questions.map((data, key) => {
               return (
                 <div>
                   <p key={key}>
                     คำถามที่ {data.id} : {data.content}
                   </p>
-                  <p key={key}>{data.answer}</p>
+                  <p key={key}>{data.ans_content}</p>
                 </div>
               )
             })}
@@ -46,5 +37,6 @@ class QuestionAndAnswer extends React.Component {
     )
   }
 }
+
 
 export default QuestionAndAnswer
