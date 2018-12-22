dded1256966f49a4f6e3b8035ae2da96ce2ba7cb
SandzoNight <flukemdc@gmail.com>
2018-12-22 21:13:32 +0700

    [UPDATE] Use env config in testenv.js

--- pages/testenv.js
+++ pages/testenv.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 
+import ENV from '../config/envConfig'
+
 export default class testenv extends Component {
   render () {
-    console.log('auth path', process.env.PATH_AUTH)
-    console.log('auth path', process.env.PATH_AUTH)
+    console.log('auth path', ENV.PATH_AUTH)
+    console.log('auth path', ENV.PATH_AUTH)
     return (
       <div>
-        <p>PATH_AUTH: {process.env.PATH_AUTH || 'no auth path'}</p>
-        <p>PATH_REGISTANCE: {process.env.PATH_REGISTANCE || 'no registance path'}</p>
+        <p>PATH_AUTH: {ENV.PATH_AUTH || 'no auth path'}</p>
+        <p>PATH_REGISTANCE: {ENV.PATH_REGISTANCE || 'no registance path'}</p>
       </div>
     )
   }
