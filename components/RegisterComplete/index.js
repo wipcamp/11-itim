56a4107a299905c523ea76efe966013fe69a3b74
pea <pepea.23@live.com>
2018-12-23 04:23:02 +0700

    [ADD] HandleCheckLogin

--- components/RegisterComplete/index.js
+++ components/RegisterComplete/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import Router from 'next/router'
 import Navbar from '../Core/Navbar'
 import Complete from './Complete'
 import Body from '../Core/Body'
+import CookiesService from '../../service/CookieService';
+
 
 export default class RegisComplete extends React.Component {
   state = {
@@ -11,15 +14,24 @@ export default class RegisComplete extends React.Component {
     confirm: 0
   }
   componentDidMount = async () => {
-    
+    this.handleCheckLoginState()
+  }
+  handleCheckLoginState = async() => {
+    if (await CookiesService.gettokenJWTCookie()) {
+    }else{
+        Router.push({
+          pathname: '/index'
+        })
+    }
   }
 
   render() {
     return (
       <Body visible={this.props.visible}>
         <Navbar state={this.state} />
-        <Complete name={this.state.name}/>
+        <Complete handleCheckLoginState={this.handleCheckLoginState}  name={this.state.name}/>
       </Body>
-    )
+    )          
+
   }
 }
