c6c4b6701ab8a3cf1cb56c663c2c4ab3bd31c1e8
Mild-TN <mild20091931@gmail.com>
2019-02-11 00:35:47 +0700

    [UPDATE] แก้ Link กับ จัดหน้า

--- components/RegisterComplete/Complete.js
+++ components/RegisterComplete/Complete.js
@@ -3,6 +3,7 @@ import { Card } from 'antd'
 import ButtonPrimary from '../Core/Button'
 import Headline, { Paragraph } from '../Core/Text'
 import styled from 'styled-components'
+import Link from 'next/link'
 
 const CardReponsive = styled(Card)`
     @media (max-width : 768px){
@@ -19,34 +20,45 @@ const CardReponsive = styled(Card)`
     }
   }
 `
+
+const Img = styled.img`
+  width: 10%;
+  @media (max-width : 768px) {
+    width: 20%;
+  }
+`
 export default class Complete extends React.Component {
   handleClick = () => {
     window.location('https://www.facebook.com/wipcamp/')
   }
-  render () {
+  render() {
     return (
       <div className="container-fluid">
         <div className="container">
-          <CardReponsive className="mt-5 mb-2">
-            <div className="row justify-content-center">
-              <div className="col-2"></div>
-            </div>
-            <div className="row text-center">
-              <div className="col-12 mt-3">
-                <Headline>ส่งใบสมัครค่ายสำเร็จ</Headline>
-              </div>
-              <div className="col-12 mt-3">
-                <Paragraph>
-                  {this.props.name} ได้ส่งใบสมัครค่ายสำเร็จเรียบร้อยแล้ว
-                  โปรดรอฟังผลการประกาศค่ายในวันที่ 28 มีนาคม 2562 ผ่านทาง
-                  Facebook Live นะครับ
+          <div className="row justify-content-center">
+            <CardReponsive className="mt-5 mb-2 p-2">
+              <div className="row text-center">
+                <div className="col-12 mt-3">
+                  <Img src="/static/img/correct.png" />
+                  <Headline>ส่งใบสมัครค่ายสำเร็จ</Headline>
+                </div>
+                <div className="col-12 mt-3">
+                  <Paragraph>
+                    น้อง{this.props.name} ได้ส่งใบสมัครค่ายสำเร็จเรียบร้อยแล้ว
                 </Paragraph>
+                  <Paragraph>
+                    โปรดรอฟังผลการประกาศค่ายในวันที่ 28 มีนาคม 2562 ผ่านทาง
+                    Facebook Live นะครับ
+                </Paragraph>
+                </div>
               </div>
-            </div>
-            <div className="row justify-content-center mt-5">
-              <ButtonPrimary onClick={this.handleClick}>ไปยัง Facebook Fanpage</ButtonPrimary>
-            </div>
-          </CardReponsive>
+              <div className="row justify-content-center mt-3 p-3">
+                <Link href="https://www.facebook.com/wipcamp/" >
+                  <a target="_blank"><ButtonPrimary >ไปยัง Facebook Fanpage</ButtonPrimary></a>
+                </Link>
+              </div>
+            </CardReponsive>
+          </div>
         </div>
       </div>
     )
