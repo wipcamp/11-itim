0512a2e9bb8ef1707ce97ac641ce5bf91147810b
Mild-TN <mild20091931@gmail.com>
2019-02-10 21:04:05 +0700

    [UPDATE] เปลี่ยน Progressbar

--- components/Core/ProgressBar.js
+++ components/Core/ProgressBar.js
@@ -3,6 +3,24 @@ import { Steps } from 'antd'
 import styled from 'styled-components'
 let Step = Steps.Step
 Step = styled(Step)`
+  &.ant-steps-item-process .ant-steps-item-icon > .ant-steps-icon .ant-steps-icon-dot {
+      background: #fff;
+      border: 0.5px solid #304151;
+      .ant-steps-icon-dot {
+        background-color: #304151;
+      }
+    }
+
+    &.ant-steps-item-finish > .ant-steps-item-tail:after {
+      background-color:#304151;
+    }
+
+    &.ant-steps-item-finish .ant-steps-item-icon, &.ant-steps-item-process .ant-steps-item-icon {
+      .ant-steps-icon-dot {
+        background-color: #304151;
+      }
+    }
+
   @media (max-width: 575.98px) {
     & .ant-steps-item-content {
       display:none;
@@ -36,20 +54,20 @@ Step = styled(Step)`
 
     &.ant-steps-item-process .ant-steps-item-icon > .ant-steps-icon .ant-steps-icon-dot {
       background: #fff;
-      border: 1px solid #00B2FF;
+      border: 1px solid #304151;
       .ant-steps-icon-dot {
-        background-color: #00B2FF;
+        background-color: #304151;
       }
     }
 
     &.ant-steps-item-finish > .ant-steps-item-tail:after {
-      background-color: #00B2FF;
+      background-color:#304151;
     }
 
     &.ant-steps-item-finish .ant-steps-item-icon, &.ant-steps-item-process .ant-steps-item-icon {
-      border: 1px solid #00B2FF;
+      border: 1px solid #304151;
       .ant-steps-icon-dot {
-        background-color: #00B2FF;
+        background-color: #304151;
       }
     }
     
@@ -61,9 +79,9 @@ export default class ProgressBar extends React.Component {
     questions: []
   }
   pageCountByQuestion = () => {
-    let count =  Math.ceil(this.props.questions.length / 3)
+    let count = Math.ceil(this.props.questions.length / 3)
     for (let index = 0; index < count; index++) {
-      this.state.questions.splice(index,count,'')
+      this.state.questions.splice(index, count, '')
     }
   }
 
@@ -72,8 +90,8 @@ export default class ProgressBar extends React.Component {
       <Fragment>
         <Steps progressDot current={this.props.current} onClick={this.pageCountByQuestion()}>
           <Step title="ข้อมูลส่วนตัว" />
-          {this.state.questions.map((data,key) => {
-            return <Step description={`ตอบคำถามส่วนที่ ${key+1}`} key={key} />
+          {this.state.questions.map((data, key) => {
+            return <Step description={`ตอบคำถามส่วนที่ ${key + 1}`} key={key} />
           })}
           <Step title="ยืนยัน" />
         </Steps>
