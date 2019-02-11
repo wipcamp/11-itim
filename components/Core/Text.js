4bd2594724596d36b51a3ebdba55e4d57d0135b3
Mild-TN <mild20091931@gmail.com>
2019-02-11 14:44:07 +0700

    [UPDATE] ใส่ font

--- components/Core/Text.js
+++ components/Core/Text.js
@@ -1,23 +1,29 @@
 import styled from 'styled-components'
 import config from '../../config/fonts'
+
 const Headline = styled.h1`
+  font-family: 'Sarabun', sans-serif;
   font-size: ${config.headline};
   font-weight: bold;
 `
 
 export const Subtitle = styled.h1`
+  font-family: 'Sarabun', sans-serif;
   font-size: ${config.subtitle};
 `
 
 export const Paragraph = styled.h1`
+  font-family: 'Sarabun', sans-serif;
   font-size: ${config.paragraph};
 `
 
 export const ParagraphBold = styled(Paragraph)`
+  font-family: 'Sarabun', sans-serif;
   font-weight: bold;
 `
 
 export const Small = styled.h5`
+  font-family: 'Sarabun', sans-serif;
   font-size: ${config.small};
 `
 
