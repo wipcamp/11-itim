d9e02bebc3b913ba0451362c299440259570bf04
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-03-28 22:07:04 +0700

    [UPDATE] push ui update for upload

--- components/Pass/index.js
+++ components/Pass/index.js
@@ -3,6 +3,7 @@ import Navbar from '../Core/Navbar'
 import TestUpload from './testupload'
 import { Card } from 'antd'
 import Headline, { Paragraph } from '../Core/Text'
+import TablePass from './TablePass'
 import styled from 'styled-components'
 
 const Subtitle = styled.h2`
@@ -19,19 +20,40 @@ export default class Pass extends React.Component {
           ขอแสดงความยินดีกับน้องไอติมที่ผ่านรอบคัดเลือกเข้าค่าย WIP Camp #11
           ขอให้น้องทำตามขั้นตอนดังต่อไปนี้ เพื่อยืนยันสิทธิ์ครับ
         </Paragraph>
-        <Card>
-          <Paragraph>อัพโหลด ปพ.1</Paragraph>
-          <Paragraph>อัพโหลด ใบขออนุญาตผู้ปกครอง</Paragraph>
-          <Paragraph>อัพโหลด สลิป</Paragraph>
-          <Paragraph>เลือกไซส์เสื้อ</Paragraph>
-          <Paragraph>เลือกวิธีการเดินทาง</Paragraph>
+        <Card className="my-5">
+          <div className="row">
+            <div className="col-12">
+              <Paragraph>อัพโหลด ปพ.1</Paragraph>
+              <Paragraph>อัพโหลด ใบขออนุญาตผู้ปกครอง</Paragraph>
+              <Paragraph>อัพโหลด สลิป</Paragraph>
+              <Paragraph>เลือกไซส์เสื้อ</Paragraph>
+              <Paragraph>เลือกวิธีการเดินทาง</Paragraph>
+            </div>
+          </div>
         </Card>
-        <Card>
+        <Card className="my-5">
           <p>sdasdasd</p>
         </Card>
-        <Card>
+        <Card className="my-5">
           <Subtitle>อัพโหลดเอกสาร</Subtitle>
-          <TestUpload />
+          <div className="row my-3">
+            <div className="col-12 col-md-4">
+              <TestUpload />
+            </div>
+            <div className="col-12 col-md-4">
+              <TestUpload />
+            </div>
+            <div className="col-12 col-md-4">
+              <TestUpload />
+            </div>
+          </div>
+          <Subtitle>ไซส์เสื้อ</Subtitle>
+          <div className="row">
+            <div className="col-12">
+              <TablePass />
+            </div>
+          </div>
+          <Subtitle>วิธีการเดินทางมาค่าย</Subtitle>
         </Card>
       </div>
     )
