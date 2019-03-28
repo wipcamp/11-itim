864726a86878e60c5fca7c1ec09bc89a47b9d29d
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-03-29 00:23:39 +0700

    [UPDATE] itim_passing with bg

--- components/Pass/index.js
+++ components/Pass/index.js
@@ -7,6 +7,8 @@ import TablePass from './TablePass'
 import config from '../../config/fonts'
 import styled from 'styled-components'
 import Button from '../Core/Button'
+import BG from '../Core/Bg'
+import colors from '../../config/colors'
 
 const Subtitle = styled.h2`
   font-size: 20px;
@@ -18,73 +20,85 @@ const Radio = styled(DefualtRadio)`
 font-size: ${config.paragraph};
 `
 
+const BgColors = styled.div`
+  background: ${colors.bgcolor};
+  @media (max-width : 768px) {
+    height: 100%;
+  }
+`
+
 const RadioGroup = DefualtRadio.Group
 
 export default class Pass extends React.Component {
   render () {
     return (
-      <div className="container">
-        <Navbar />
-        <Card className="my-5">
-          <Headline className="text-center">Congratulation !</Headline>
-          <Paragraph className="text-center">
+      <BgColors>
+        <div className="container">
+          <Navbar />
+          <Card className="my-5">
+            <Headline className="text-center">Congratulation !</Headline>
+            <Paragraph className="text-center">
           ขอแสดงความยินดีกับน้องไอติมที่ผ่านรอบคัดเลือกเข้าค่าย WIP Camp #11
           ขอให้น้องทำตามขั้นตอนดังต่อไปนี้ เพื่อยืนยันสิทธิ์ครับ
-          </Paragraph>
-          <Card className="my-5">
-            <div className="row my-3">
+            </Paragraph>
+            <Card className="my-5">
+              <div className="row my-3">
+                <div className="col-12">
+                  <Paragraph>อัพโหลด ปพ.1</Paragraph>
+                  <Paragraph>อัพโหลด ใบขออนุญาตผู้ปกครอง</Paragraph>
+                  <Paragraph>อัพโหลด สลิป</Paragraph>
+                  <Paragraph>เลือกไซส์เสื้อ</Paragraph>
+                  <Paragraph>เลือกวิธีการเดินทาง</Paragraph>
+                </div>
+              </div>
+            </Card>
+            <Card className="my-5">
+              <p>sdasdasd</p>
+            </Card>
+            <Card className="my-5">
+              <Subtitle>อัพโหลดเอกสาร</Subtitle>
+              <div className="row my-3">
+                <div className="col-12 col-md-4">
+                  <TestUpload />
+                </div>
+                <div className="col-12 col-md-4">
+                  <TestUpload />
+                </div>
+                <div className="col-12 col-md-4">
+                  <TestUpload />
+                </div>
+              </div>
+              <Subtitle>ไซส์เสื้อ</Subtitle>
+              <div className="row my-3">
+                <div className="col-12">
+                  <TablePass />
+                </div>
+              </div>
+              <Subtitle>วิธีการเดินทางมาค่าย</Subtitle>
+              <div className="row my-3">
+                <div className="col-12">
+                  <RadioGroup >
+                    <Radio value={1}>เดินทางมาเอง</Radio> <br/>
+                    <Radio value={2}>ให้พี่ค่ายรอรับที่ หัวลำโพง</Radio> <br/>
+                    <Radio value={3}>ให้พี่ค่ายรอรับที่ สายใต้ใหม่</Radio> <br/>
+                    <Radio value={4}>ให้พี่ค่ายรอรับที่ อนุเสาวรีย์ชัยสมรภูมิ</Radio> <br/>
+                    <Radio value={5}>ให้พี่ค่ายรอรับที่ หมอชิต</Radio>
+                  </RadioGroup>
+                </div>
+              </div>
+            </Card>
+            <Paragraph>โปรดตรวจสอบรายละเอียดให้เรียบร้อย หากข้อมูลที่กรอกมาเป็นเท็จทางค่ายจะตัดสิทธิ์ทันที</Paragraph>
+            <div className="row text-center">
               <div className="col-12">
-                <Paragraph>อัพโหลด ปพ.1</Paragraph>
-                <Paragraph>อัพโหลด ใบขออนุญาตผู้ปกครอง</Paragraph>
-                <Paragraph>อัพโหลด สลิป</Paragraph>
-                <Paragraph>เลือกไซส์เสื้อ</Paragraph>
-                <Paragraph>เลือกวิธีการเดินทาง</Paragraph>
+                <Button>ยืนยัน</Button>
               </div>
             </div>
           </Card>
-          <Card className="my-5">
-            <p>sdasdasd</p>
-          </Card>
-          <Card className="my-5">
-            <Subtitle>อัพโหลดเอกสาร</Subtitle>
-            <div className="row my-3">
-              <div className="col-12 col-md-4">
-                <TestUpload />
-              </div>
-              <div className="col-12 col-md-4">
-                <TestUpload />
-              </div>
-              <div className="col-12 col-md-4">
-                <TestUpload />
-              </div>
-            </div>
-            <Subtitle>ไซส์เสื้อ</Subtitle>
-            <div className="row my-3">
-              <div className="col-12">
-                <TablePass />
-              </div>
-            </div>
-            <Subtitle>วิธีการเดินทางมาค่าย</Subtitle>
-            <div className="row my-3">
-              <div className="col-12">
-                <RadioGroup >
-                  <Radio value={1}>เดินทางมาเอง</Radio> <br/>
-                  <Radio value={2}>ให้พี่ค่ายรอรับที่ หัวลำโพง</Radio> <br/>
-                  <Radio value={3}>ให้พี่ค่ายรอรับที่ สายใต้ใหม่</Radio> <br/>
-                  <Radio value={4}>ให้พี่ค่ายรอรับที่ อนุเสาวรีย์ชัยสมรภูมิ</Radio> <br/>
-                  <Radio value={5}>ให้พี่ค่ายรอรับที่ หมอชิต</Radio>
-                </RadioGroup>
-              </div>
-            </div>
-          </Card>
-          <Paragraph>โปรดตรวจสอบรายละเอียดให้เรียบร้อย หากข้อมูลที่กรอกมาเป็นเท็จทางค่ายจะตัดสิทธิ์ทันที</Paragraph>
-          <div className="row text-center">
-            <div className="col-12">
-              <Button>ยืนยัน</Button>
-            </div>
-          </div>
-        </Card>
-      </div>
+        </div>
+        <BG />
+
+      </BgColors>
+
     )
   }
 }
