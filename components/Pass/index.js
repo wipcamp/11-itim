ff4cf2fd5b6e78d1bfb7453dbd5879e2bbf680c2
fame <thinnapat.2542@mail.kmutt.ac.th>
2019-03-28 09:36:16 +0700

    [UPDATE] merge

--- components/Pass/index.js
+++ components/Pass/index.js
@@ -2,40 +2,37 @@ import React from 'react'
 import Navbar from '../Core/Navbar'
 import TestUpload from './testupload'
 import { Card } from 'antd'
+import Headline, { Paragraph } from '../Core/Text'
+import styled from 'styled-components'
+
+const Subtitle = styled.h2`
+  font-size: 24px;
+`
 
 export default class Pass extends React.Component {
   render () {
     return (
       <div className="container">
         <Navbar />
-        <Card title="Card title">
-          <p
-            style={{
-              fontSize: 14,
-              color: 'rgba(0, 0, 0, 0.85)',
-              marginBottom: 16,
-              fontWeight: 500
-            }}
-          >
-      Group title
-          </p>
-          <Card
-            type="inner"
-            title="Inner Card title"
-            extra={<a href="#">More</a>}
-          >
-      Inner Card content
-          </Card>
-          <Card
-            style={{ marginTop: 16 }}
-            type="inner"
-            title="Inner Card title"
-            extra={<a href="#">More</a>}
-          >
-      Inner Card content
-          </Card>
+        <Headline className="text-center">Congratulation !</Headline>
+        <Paragraph className="text-center">
+          ขอแสดงความยินดีกับน้องไอติมที่ผ่านรอบคัดเลือกเข้าค่าย WIP Camp #11
+          ขอให้น้องทำตามขั้นตอนดังต่อไปนี้ เพื่อยืนยันสิทธิ์ครับ
+        </Paragraph>
+        <Card>
+          <Paragraph>อัพโหลด ปพ.1</Paragraph>
+          <Paragraph>อัพโหลด ใบขออนุญาตผู้ปกครอง</Paragraph>
+          <Paragraph>อัพโหลด สลิป</Paragraph>
+          <Paragraph>เลือกไซส์เสื้อ</Paragraph>
+          <Paragraph>เลือกวิธีการเดินทาง</Paragraph>
         </Card>
-        <TestUpload></TestUpload>
+        <Card>
+          <p>sdasdasd</p>
+        </Card>
+        <Card>
+          <Subtitle>อัพโหลดเอกสาร</Subtitle>
+          <TestUpload />
+        </Card>
       </div>
     )
   }
