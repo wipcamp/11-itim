8f97fb807f9bd419f1a53d324460008bc60c74cc
pepea <pepea.23@live.com>
2019-03-31 22:24:03 +0700

    [ADD] camperservice

---
+++ service/CamperService.js
@@ -0,0 +1,10 @@
+import api from '../utils/apiCamperService'
+import Cookies from './CookieService'
+
+const AuthService = {
+  submitData: async (request) => {
+
+  }
+}
+
+export default AuthService
