4447e5e0c1eba9c7eef29d2ad0dab65841c74ef1
pepea23 <32633442+pepea23@users.noreply.github.com>
2019-05-22 22:53:50 +0700

    [UPDATE] BG หน้าโหลดเอกสาร

--- pages/downloadconfrim.js
+++ pages/downloadconfrim.js
@@ -1,11 +1,28 @@
 import React, { Fragment } from 'react';
 import Download from './../components/Download/index'
+import styled from 'styled-components'
+import colors from '../config/colors'
+import Bg from '../components/Core/Bg'
+
+
+const BgColors = styled.div`
+  background: ${colors.bgcolor};
+  overflow:hidden;
+  @media (max-width : 768px) {
+    height: 100vh;
+  }
+`
+
+
 class downloadconfrim extends React.Component {
 
     render() {
         return (
             <Fragment>
-                <Download/>
+                   <BgColors>
+                      <Download/>
+                    <Bg position="absolute"/>
+                  </BgColors>
             </Fragment>
         );
     }
